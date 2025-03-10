ed22a34bf8110fe5ceb517b4a3993371e9ca6cf1
Al Naheyan <officialk2000@gmail.com>
2025-03-10 14:48:33 -0400

    deploy files

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8080/api/:path*" // Flask in development
+            : "/api/", // For production
+      },
+    ];
+  },
 };
 
 export default nextConfig;
